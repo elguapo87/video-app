d6d2956427f0c840d301b1d9492d5c380228bb32
elguapo87 <pglogovac@gmail.com>
2025-04-16 23:23:43 +0200

    Setup mongodb connection

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
+import "dotenv/config";
 import express from "express";
+import { connectDB } from "./config/db.js";
 
 // Initialize Express
 const app = express();
 const port = process.env.PORT || 8800;
 
+// DB Connection
+connectDB();
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
-})
+});
 
