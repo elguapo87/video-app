845d37ccf1bb3dc91a4d3e9daaf144d226e49c68
elguapo87 <pglogovac@gmail.com>
2025-04-17 14:57:42 +0200

    Create addView, subscribedVideo functions & create videoSlice

--- server/controllers/videoController.js
+++ server/controllers/videoController.js
@@ -1,4 +1,6 @@
 import videoModel from "../models/videoModel.js";
+import userModel from "../models/userModel.js";
+import { createError } from "../error.js";
 
 export const addVideo = async (req, res, next) => {
     const newVideo = new videoModel({ userId: req.user.id, ...req.body });
@@ -6,6 +8,68 @@ export const addVideo = async (req, res, next) => {
     try {
         const savedVideo = await newVideo.save();
         res.status(200).json(savedVideo);
+
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const updateVideo = async (req, res, next) => {
+    try {
+        const video = await videoModel.findById(req.params.id);
+        if(!video) return next(createError(404, "Video not found!"));
+
+        if(req.user.id === video.userId) {
+            const updateVideo = await videoModel.findByIdAndUpdate(req.params.id, {
+                $set: req.body
+            }, { new: true });
+
+            res.status(200).json(updateVideo);
+
+        } else {
+            next(createError(403, "You can update only your video"));
+        }
+        
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const deleteVideo = async (req, res, next) => {
+    try {
+        const video = await videoModel.findById(req.params.id);
+        if (!video) return next(createError(404, "Video not found!"));
+
+        if (req.user.id === video.userId) {
+            await videoModel.findByIdAndDelete(req.params.id);
+            res.status(200).json("Video has been deleted.");
+
+        } else {
+            next(createError(403, "You can delete only your video"));
+        }
+        
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const getVideo = async (req, res, next) => {
+    try {
+        const video = await videoModel.findById(req.params.id);
+        res.status(200).json(video);
+
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const addView = async (req, res, next) => {
+    try {
+        await videoModel.findByIdAndUpdate(req.params.id, {
+            $inc: { views: 1 } 
+        });
+
+        res.status(200).json("The view has been increased.");
 
     } catch (err) {
         next(err);
@@ -32,6 +96,23 @@ export const trendVideos = async (req, res, next) => {
     }
 };
 
+export const sub = async (req, res, next) => {
+    try {
+        const user = await userModel.findById(req.user.id);
+        const subscribedChannels = user.subscribedChannels;
+
+        const list = await Promise.all(
+            subscribedChannels.map((channelId) => {
+                return videoModel.find({ userId: channelId });
+            })
+        );
+        
+        res.status(200).json(list.flat().sort((a, b) => b.createdAt - a.createdAt));
+
+    } catch (err) {
+        next(err);
+    }
+};
 
 export const search = async (req, res, next) => {
     try {
@@ -58,3 +139,13 @@ export const searchByTags = async (req, res, next) => {
         next(err);
     }
 };
+
+export const getVideosByUser = async (req, res, next) => {
+    try {
+        const videos = await videoModel.find({ userId: req.params.userId });
+        res.status(200).json(videos);
+    } catch (err) {
+        next(err);
+    }
+};
+
--- server/routes/videoRoute.js
+++ server/routes/videoRoute.js
@@ -1,17 +1,29 @@
 import express from "express";
 import { verifyToken } from "../verifyToken.js";
-import { addVideo, randomVideos, search, searchByTags, trendVideos } from "../controllers/videoController.js";
+import { addVideo, addView, deleteVideo, getVideo, getVideosByUser, randomVideos, search, searchByTags, sub, trendVideos, updateVideo } from "../controllers/videoController.js";
 
 const router = express.Router();
 
 router.post("/", verifyToken, addVideo);
 
-router.get("/search", search);
+router.put("/:id", verifyToken, updateVideo);
 
-router.get("/tags", searchByTags);
+router.delete("/:id", verifyToken, deleteVideo);
+
+router.get("/find/:id", getVideo);
+
+router.put("/view/:id", addView);
 
 router.get("/random", randomVideos);
 
 router.get("/trend", trendVideos);
+
+router.get("/sub", verifyToken, sub);
+
+router.get("/search", search);
+
+router.get("/tags", searchByTags);
+
+router.get("/user/:userId", getVideosByUser);
 
 export default router;
