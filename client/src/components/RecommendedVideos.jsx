0eb77548a0e144f0ae539f7fa0310c01159a6690
elguapo87 <pglogovac@gmail.com>
2025-04-17 16:16:12 +0200

    Replace hardcoded data in RecommendedVideos component with real data

--- client/src/components/RecommendedVideos.jsx
+++ client/src/components/RecommendedVideos.jsx
@@ -1,16 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import apiRequest from '../lib/apiRequest';
 import Card from './Card';
 
 const Container = styled.div`
     flex: 2;
 `;
 
-const RecommendedVideos = () => {
+const RecommendedVideos = ({ tags }) => {
     
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+        try {
+            const res = await apiRequest.get(`/videos/tags?tags=${tags}`);
+            setVideos(res.data);
+        } catch (err) {
+            console.error("Failed to fetch videos", err);
+        }
+    };
+    fetchVideos();
+  }, [tags])
+
   return (
     <Container>
-        <Card type="sm" />
+      {videos.map((video) => (
+        <Card key={video._id} type="sm" video={video} />
+      ))}
     </Container>
   )
 }
