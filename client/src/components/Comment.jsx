9abbbe3389ba6038364ed92c30de4ee9299415f2
elguapo87 <pglogovac@gmail.com>
2025-04-17 16:11:19 +0200

    Replace hardcoded data with real data in Comments & Comment components

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,55 +1,106 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import apiRequest, { url } from "../lib/apiRequest";
 import { assets } from "../assets/assets";
+import { format } from "timeago.js";
+import { useSelector } from "react-redux";
 
 const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  /* gap: 10px;
+  margin: 30px 0px; */
+`;
+
+const Wrapper = styled.div`
   display: flex;
   gap: 10px;
   margin: 30px 0px;
 `;
 
 const Avatar = styled.img`
-    width: 50px;
-    height: 50px;
+    width: max(3vw, 24px);
+    height: max(3vw, 24px);
     border-radius: 50%;
-`
+`;
 
 const Details = styled.div`
     display: flex;
     flex-direction: column;
     gap: 10px;
     color: ${({ theme }) => theme.text};
-`
+`;
 
 const Name = styled.span`
-  font-size: 13px;
+  font-size: max(0.95vw, 13px);
   font-weight: 500; 
-`
+`;
+
 
 const Date = styled.span`
-  font-size: 12px;
+  font-size: max(0.85vw, 12px);
   font-weight: 400;
   color: ${({ theme }) => theme.textSoft};
   margin-left: 5px;
-`
+`;
 
 const Text = styled.span`
-  font-size: 14px;
-`
+  font-size: max(0.85vw, 12px);
+`;
 
-const Comment = () => {
+const DeleteBtn = styled.button`
+  padding: max(0.3vw, 3px) max(0.6vw, 8px);
+  background: transparent; 
+  border: 1px solid #f00d0d;
+  color: ${({ theme }) => theme.text};
+  font-size: max(0.8vw, 11px);
+  cursor: pointer;
+`;
+
+const Comment = ({ comment, currentVideo, setComments }) => {
+  const { currentUser } = useSelector((state) => state.user);
+  const [channel, setChannel] = useState([]);
+
+  useEffect(() => {
+    const fetchChannel = async () => {
+      try {
+        const res = await apiRequest.get(`/users/find/${comment.userId}`);
+        setChannel(res.data);
+      } catch (err) {
+        console.error("Error fetching channel", err);
+      }
+    };
+    fetchChannel();
+  }, [comment.userId]);  
+
+  
+  
+  const handleDelete = async () => {
+    try {
+      await apiRequest.delete(`/comments/${comment._id}`)
+      setComments((prev) => prev.filter((c) => c._id !== comment._id));
+
+    } catch (err) {
+      console.error("Error deleting comment", err);
+    }
+  };
+
+  const isOwner = currentUser && (currentUser._id === comment.userId || currentUser._id === currentVideo.userId);
+
   return (
     <Container>
-      <Avatar src={assets.noavatar} />
-      <Details>
-        <Name>John Doe <Date>1 day ago</Date></Name>
-        <Text>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel, ex
-          laboriosam ipsam aliquam voluptatem perferendis provident modi, sequi
-          tempore reiciendis quod, optio ullam cumque? Quidem numquam sint
-          mollitia totam reiciendis?
-        </Text>
-      </Details>
-
+      <Wrapper>
+        <Avatar src={channel.fromGoogle ? channel.img : channel.img ? `${url}/images/${channel.img}` : assets.noavatar} />
+        <Details>
+          <Name>{channel.name} <Date>{format(channel.createdAt)}</Date></Name>
+          <Text>
+            {comment.description}
+            
+          </Text>
+        </Details>
+      </Wrapper>
+      {isOwner && (<DeleteBtn onClick={handleDelete}>Delete</DeleteBtn>)}
     </Container>
   )
 }
--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,47 +1,116 @@
 import styled from "styled-components";
 import Comment from "./Comment";
-import {assets} from "../assets/assets";
+import { useEffect, useState } from "react";
+import apiRequest, { url } from "../lib/apiRequest";
+import { useSelector } from "react-redux";
+import { assets } from "../assets/assets";
 
-const Container = styled.div``;
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
 
 const NewComment = styled.div`
     display: flex;
     align-items: center;
-    gap: 10px;
-`
+    gap: max(0.6vw, 5px);
+`;
 
 const Avatar = styled.img`
-    width: 50px;
-    height: 50px;
+    width: max(3vw, 24px);
+    height: max(3vw, 24px);
     border-radius: 50%;
-`
+`;
 
-const Input = styled.input`
+const InputBox = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 3px;
+`;
+
+const Input = styled.textarea`
     border: none;
-    border-bottom: 1px solid ${({theme}) => theme.soft};
+    border-bottom: 1px solid ${({ theme }) => theme.soft};
     background: transparent;
     outline: none;
+    font-size: max(0.85vw, 12px);
     padding: 5px;
     width: 100%;
     color: ${({ theme }) => theme.text};
-`
+    /* resize: none;  */
+    line-height: 1.5; 
+`;
 
-const Comments = () => {
-  return (
-    <Container>
-        <NewComment>
-            <Avatar src={assets.noavatar} />
-            <Input placeholder="Add a comment..." />
-        </NewComment>
-        <Comment />
-        <Comment />
-        <Comment />
-        <Comment />
-        <Comment />
-        <Comment />
-        <Comment />
-    </Container>
-  )
-}
+const SendBtn = styled.button`
+    padding: max(0.4vw, 3px) max(1vw, 8px);
+    font-size: 13px;
+    background: transparent;
+    border: 1px solid ${({ theme }) => theme.text};;
+    color: ${({ theme }) => theme.text};;
+    border-radius: 3px;
+    font-size: max(0.8vw, 11px);
+    font-weight: 500;
+    cursor: pointer;
+    margin-top: 4px;
+`;
 
-export default Comments
+const Comments = ({ currentVideo }) => {
+    const { currentUser } = useSelector((state) => state.user);
+    const [comments, setComments] = useState([]);
+    const [description, setDescription] = useState("");
+
+    useEffect(() => {
+        const fetchComments = async () => {
+            try {
+                const res = await apiRequest.get(`/comments/${currentVideo._id}`);
+                setComments(res.data);
+            } catch (err) {
+                console.error("Error fetching comments", err);
+            }
+        };
+        fetchComments();
+    }, [currentVideo?._id]);
+
+    const handleSendComment = async () => {
+        if (currentUser) {
+            try {
+                const res = await apiRequest.post("/comments", {
+                    userId: currentUser?._id,
+                    videoId: currentVideo?._id,
+                    description
+                })
+                setComments((prev) => [...prev, res.data]);
+                setDescription("")
+            } catch (err) {
+                console.error("Error sending comment", err);
+            }
+
+        } else {
+            alert("Please log in to send comment.");
+            setDescription("");
+        }
+    };
+
+    return (
+        <Container>
+            <NewComment>
+                <Avatar
+                    src={currentUser ?
+                        (currentUser?.fromGoogle ? currentUser?.img : currentUser?.img ? `${url}/images/${currentUser?.img}` : assets.noavatar)
+                        : assets.noavatar}
+                />
+                <InputBox>
+                    <Input placeholder="Add a comment..." rows="1" onChange={(e) => setDescription(e.target.value)} value={description} />
+                    {description && <SendBtn onClick={handleSendComment}>Send</SendBtn>}
+                </InputBox>
+            </NewComment>
+            {comments.map((comment) => (
+                <Comment key={comment?._id} comment={comment} currentVideo={currentVideo} setComments={setComments} />
+            ))}
+        </Container>
+    );
+};
+
+export default Comments;
